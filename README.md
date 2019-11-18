1d239048e8351152cb6728e564405cac38d2cf89
Kathrin Eichinger <kathrin801@gmail.com>
2019-11-18 18:11:39 +0000

    plane rotation

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# project-x
+
+### ![GA](https://cloud.githubusercontent.com/assets/40461/8183776/469f976e-1432-11e5-8199-6ac91363302b.png) General Assembly, Software Engineering Immersive
+# Project #X: Skystalker
+ 
+
+## Overview
+
+This is a bonus project of the software engineering immersive course at GA London. The assignment was to create a **react app** using data from any API of our choice. The assignment was to completed **individually** over one *weekend*. 
+
+Using live air traffic data from the OpenSky API, my app shows the current number of airplanes above London and tries to put this into perspective by comparing it to the total number of planes in the sky above entire countries.
+
+You can launch the game on GitHub pages [here](https://katheich.github.io/project-x/), or find the GitHub repo [here](https://github.com/katheich/project-x).
+
+## Brief
+
+- Create a react app that talks to an API and displays some data
+- Use any API you like
+- Use some new libraries in your creation
+- Host it on Github pages
+
+## Technologies used
+
+- HTML
+- CSS
+- JavaScript (ES6)
+- React.js
+- OpenSky API
+- Git and GitHub
+- BoundingBox
+- Google Fonts
+
+## Approach
+
+## Screenshots
+
+## Bugs 
+
+## Future features
+
+## Lessons learned
+
+## Artwork and credit
+
+[OpenSky](https://opensky-network.org/apidoc/) by The OpenSky Network
+
+[BoundingBox](https://boundingbox.klokantech.com/) by Klokan Technologies
+
+[London skyline](https://pixabay.com/vectors/london-skyline-silhouette-city-155996/) from OpenClipart-Vectors via pixabay
+
+
--- src/components/london.js
+++ src/components/london.js
@@ -1,49 +1,73 @@
 import React from 'react'
+import anime from 'animejs'
+ 
+class London extends React.Component {
 
-const London = (dataLondon) => {
+  constructor() {
+    super()
+  }
 
-  // Turn into an array
-  const dataArray = Array.from(dataLondon.london)
+  getLength(dataLondon) {
+    return Array.from(dataLondon).length
+  }
+  
+  getOrigins(dataLondon) {
+    const dataArray = Array.from(dataLondon)
+    
+    return dataArray
+      .reduce((sum, elem) => {
+        const origin = elem[2].toString()
+        const index = sum.map(e => e.country).indexOf(origin)
+        if ( index > -1) {
+          sum[index].count += 1
+          return sum
+        } else {
+          sum.push({
+            country: origin,
+            count: 1
+          })
+          return sum
+        }
+      }, [])
+      .map((country) => {
+        const newCountry = country
+        newCountry.perc = Math.round((country.count) / dataArray.length * 100)
+        return newCountry
+      })
+      .sort(function(a, b) { return b.count - a.count } )
+  }
+  
 
-  // Get origin stats
-  const summary = dataArray
-    .reduce((sum, elem) => {
-      const origin = elem[2].toString()
-
-      const index = sum.map(e => e.country).indexOf(origin);
-      if ( index > -1) {
-        sum[index].count += 1
-        return sum
-      } else {
-        sum.push({
-          country: origin,
-          count: 1
-        })
-        return sum
-      }
-    }, [])
-    .map((country) => {
-      const newCountry = country
-      newCountry.perc = Math.round((country.count) / dataArray.length * 100)
-      return newCountry
-    })
-    .sort(function(a, b) { return b.count - a.count } )
+  // componentDidMount() {
+  //   anime({
+  //     targets: '#totalCount',
+  //     rotate: '1turn',
+  //     duration: 5000,
+  //     loop: true,
+  //     easing: 'easeInOutExpo'
+  //   })
+  // }
 
   // return finished javascripts
-  return (<div id={'london'}>
-    <div id={'totalCount'}>{dataArray.length}</div>
-    <div id={'origins'}>
-      <h4>Coming from ...</h4>
-      <div id={'origin-list'}>
-        {summary.map((origin, i) => {
-          return <div key={i}>
-            {`${i + 1}. ${origin.country}: ${origin.perc}%`}
-          </div>
-        })}
+  
+  render() {
+    return <div id={'london'}>
+      <div id={'totalCount'}>
+        <div id={'count-number'}>
+          {this.getLength(this.props.london)}
+        </div>
       </div>
-    </div>
-    
-  </div>)
+      <div id={'origins'}>
+        <h4>Coming from ...</h4>
+        <div id={'origin-list'}>
+          {this.getOrigins(this.props.london).map((origin, i) => {
+            return <div key={i}>
+              {`${i + 1}. ${origin.country}: ${origin.perc}%`}
+            </div>
+          })}
+        </div>
+      </div>
+    </div>}
 }
 
 export default London
